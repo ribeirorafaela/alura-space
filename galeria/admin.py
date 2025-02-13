6da0f83fe09e43b11c65e039289663fadd861782
Rafaela Silva Ribeiro <rafaela22ribeirovv@hotmail.com>
2025-02-13 08:56:29 -0300

    Incluindo features de imagens no admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,8 +3,11 @@ from galeria.models import Fotografia
 
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'legenda')
+    list_display = ('id', 'nome', 'legenda', 'publicada')
     list_display_links = ('id', 'nome')
     search_fields = ('nome', 'id')
+    list_filter = ('categoria',)
+    list_per_page = 10
+    list_editable = ('publicada',)
 
 admin.site.register(Fotografia, ListandoFotografias)
